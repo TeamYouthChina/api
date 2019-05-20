bfcda05c49ed77c827f80cdee88295223d75f6d0
Zhicheng Zhang <zzc-tongji@users.noreply.github.com>
2019-05-20 11:33:23 +0800

    prepare

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -u
+SCRIPT_PATH=`cd "$(dirname "$0")"; pwd -P`
+
+cd ${SCRIPT_PATH}
+git checkout master
+git pull origin master:master
+git remote update --prune
+yes | \cp ./git-hooks/* .git/hooks/
